083f526f92ccab5a20f1f71ce51c17689144c726
Glitch (hello-express) <none>
2019-03-13 14:59:38 +0000

    🌊🐡 Checkpoint ./index.js:766712/1533

--- index.js
+++ index.js
@@ -33,19 +33,39 @@ X.get('/', function(req, res) {
 X.use(express.static('public'));
 
 
-wss.on('connection', (ws) => {
+function onConnection(ws) {
   var id = randomId();
   send([ws], {type: 'connection', id});
   send([ws], {type: 'connections', ids: people.keys()});
   send(people.values(), {type: 'connection', id});
   people.set(id, ws);
   console.log(id+' just connected');
-  ws.on('close', () => {
-    var id = Map.keyOf(people, ws);
-    people.delete(id);
-    send(people.values(), {type: 'close', id});
-    console.log(id+' just closed');
-  });
+};
+
+function onClose(ws) {
+  var id = Map.keyOf(people, ws);
+  people.delete(id);
+  send(people.values(), {type: 'close', id});
+  console.log(id+' just closed');
+};
+
+function onRename(ws, req) {
+  var {id, value} = req;
+  if(people.has(value)) return send([ws], {type: 'rename', value: 'another '+value+''});
+  send(people.values(), {type: 'rename', id, value});
+  people.delete(id);
+  people.set(value, ws);
+  console.log(id+' rename to '+value);
+};
+
+function onMessage(ws, req) {
+  var {id, value} = req;
+  if(!people.has(id)) return send();
+};
+
+wss.on('connection', (ws) => {
+  onConnection(ws);
+  ws.on('close', () => onClose(ws));
   ws.on('message', (msg) => {
     var req = JSON.parse(msg);
     
