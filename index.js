01e9408cdf8b43174ddccac9e90763c481ba5f14
Glitch (hello-express) <none>
2019-03-13 14:07:29 +0000

    🦄👚 Checkpoint ./package.json:766712/873 ./index.js:766712/286

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const WebSocket = require('ws');
 const http = require('http');
 
 
 const E = process.env;
 const X = express();
 const server = http.createServer(X);
+const wss = new WebSocket.Server({server});
 
 
-X.use(express.static('public'));
-X.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+X.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
+X.use(express.static('public'));
 
 
 server.listen(E.PORT||80, () => {
