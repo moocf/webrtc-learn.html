6c66acc07f8cf5ed9c6554504a474f3ea5e95158
Glitch (hello-express) <none>
2019-03-13 14:37:33 +0000

    🎃😇 Checkpoint ./index.js:766712/551 ./package.json:766712/970

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const randomName = require('adj-noun');
 const express = require('express');
 const WebSocket = require('ws');
 const http = require('http');
@@ -12,6 +13,13 @@ const wss = new WebSocket.Server({server});
 var people = new Map();
 
 
+function broadcast(conns, res) {
+  var msg = JSON.stringify(res);
+  for(var conn of conns)
+    conn.send(msg);
+};
+
+
 X.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -20,9 +28,12 @@ X.use(express.static('public'));
 
 wss.on('connection', (ws) => {
   console.log('someone just connected');
+  var id = 
+  broadcast(people.values(), {type: 'connection', id});
   ws.send({id: people.size()});
   ws.on('close', () => {
-    people.
+    var id = Map.keyOf(people, ws);
+    broadcast(people.values(), {type: 'close', id});
   });
   ws.on('message', (msg) => {
     console.log('got a message');
